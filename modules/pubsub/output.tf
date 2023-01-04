7fd13ee2637c22ab0c2693bab0b6c55482213e48
ozgur.donmez <ozgur.donmez@yemeksepeti.com>
2023-01-04 22:39:36 +0300

    use underscore in variable names

--- modules/pubsub/output.tf
+++ modules/pubsub/output.tf
@@ -1,9 +1,9 @@
-output "topic-name" {
+output "topic_name" {
   description = "the name of the topic created"
   value       = local.topic_name
 }
 
-output "subscription-name" {
+output "subscription_name" {
   description = "the name of the subscription created"
   value       = local.subscription_name
 }

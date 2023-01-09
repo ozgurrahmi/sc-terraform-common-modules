91fb94c2c0d0fbed249887a687f1e581029206e5
ozgur.donmez <ozgur.donmez@yemeksepeti.com>
2023-01-09 15:43:32 +0300

    create cloud function module

---
+++ modules/cloudfunction/main.tf
@@ -0,0 +1,38 @@
+locals {
+  source_name = format("%s-cf.zip", var.name)
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = var.bucket_name
+  location = var.region_id
+  project  = var.project_id
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = local.source_name
+  bucket = google_storage_bucket.bucket.name
+  source = local.source_name
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name                  = var.name
+  runtime               = var.runtime
+  project               = var.project_id
+  region                = var.region_id
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = var.topic_name
+  }
+}
+
+# IAM entry for a single user to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
---
+++ modules/cloudfunction/variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  description = "Id of the project"
+  type        = string
+}
+
+variable "region_id" {
+  description = "Name of the region"
+  type        = string
+}
+
+variable "env_name" {
+  description = "Name of the environment. It could be only uat,stg,prd"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket for the cloud function source."
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the cf"
+  type        = string
+}
+
+variable "runtime" {
+  description = "Name of the runtime"
+  type        = string
+}
+
+variable "topic_name" {
+  description = "Name of the topic which triggers this cf"
+  type        = string
+}
+

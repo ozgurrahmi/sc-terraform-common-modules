037ddbdd5f6df1c74a9b4a0db781aa8d946855a1
ozgur.donmez <ozgur.donmez@yemeksepeti.com>
2023-01-11 11:06:10 +0300

    removed subscription_name parameter

--- modules/pubsub/main.tf
+++ modules/pubsub/main.tf
@@ -1,6 +1,5 @@
 locals {
-  topic_name        = format("%s-%s", var.topic_name, var.env_name)
-  subscription_name = format("%s-%s-%s", var.topic_name, "subscription", var.env_name)
+  topic_name = format("%s-%s", var.topic_name, var.env_name)
 }
 
 resource "google_pubsub_topic" "create-topic" {

00a5dfec50c1f13fe313fba2de95a62fa859e488
ozgur.donmez <ozgur.donmez@yemeksepeti.com>
2023-01-16 11:52:22 +0300

    force destroy is set to true

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "create_bucket" {
-  name     = var.bucket_name
-  location = var.location
-  project  = var.project_id
+  name          = var.bucket_name
+  location      = var.location
+  project       = var.project_id
+  force_destroy = true
 }
